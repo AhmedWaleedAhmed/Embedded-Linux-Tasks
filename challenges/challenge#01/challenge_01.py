de8e126b3948c4a4f7635b8867bb474baff77026
AhmedWaleedAhmed <ahmed.amin98@eng-st.cu.edu.eg>
2023-08-25 14:23:13 +0300

    Minor: 1- Create the notes.txt file in a fixed location (Desktop specifically) 2- Get rid of the leading and trailing whitespace in the clipboard string.

--- challenges/challenge#01/challenge_01.py
+++ challenges/challenge#01/challenge_01.py
@@ -19,6 +19,12 @@
 # illustration: https://stackoverflow.com/questions/51662213/cant-get-pyperclip-to-use-copy-and-paste-modules-on-python3#:~:text=The%20clipboard%20is%20part%20of%20your%20GUI.%20But%20you%20don%27t%20have%20a%20GUI.%20So%20there%20is%20no%20clipboard%20to%20copy%20and%20paste%20with.%20There%20is%20no%20clipboard%20for%20pyperclip%20to%20access%2C%20so%20it%20doesn%27t%20matter%20how%20you%20try%20to%20access%20it%2C%20you%27re%20going%20to%20fail.
 # solution: sudo apt-get install xclip
 
+# FYI:
+# To run this file you will need to run it with "sudo" or you will get the following error:: "ImportError: You must be root to use this library on linux."
+# So to get the Desktop location the following command "os.path.join(os.path.expanduser('~'), 'Desktop')" will not be working because of the "sudo"
+# but we can get the user name who issued the sudo using "os.environ['SUDO_USER']"
+# Ref: https://unix.stackexchange.com/questions/11470/how-to-get-the-name-of-the-user-that-launched-sudo#:~:text=37,env%20%7C%20grep%20SUDO
+
 # File Handling in Python: https://www.freecodecamp.org/news/file-handling-in-python/#:~:text=files%20in%20Python.-,File%20Handling%20in%20Python,-File%20handling%20is
 
 # Python Get Current time: https://www.programiz.com/python-programming/datetime/current-time
@@ -30,10 +36,12 @@ import os
 import time
 
 def on_trigger():
-    cwd = os.getcwd()
-    if len(pyperclip.paste()) > 0:
-        with open(f'{cwd}/notes.txt', 'a+') as f:
-            f.write(f'[{time.strftime("%b-%d-%Y %H:%M:%S %z", time.localtime())}] :: {pyperclip.paste()}\n')
+    clipboard_content = pyperclip.paste().strip()
+    # this condition will make sure that if the user has an image in the clipboard we will not have empty lines because of it.
+    # as the pyperclip.paste() only gets the text from the clipboard.
+    if len(clipboard_content) > 0:
+        with open(f"/home/{os.environ['SUDO_USER']}/Desktop/notes.txt", 'a+') as f:
+            f.write(f'[{time.strftime("%b-%d-%Y %H:%M:%S %z", time.localtime())}] :: {clipboard_content}\n')
 
 def listen_for_shortcut():
     shortcut = "alt + shift + s"
