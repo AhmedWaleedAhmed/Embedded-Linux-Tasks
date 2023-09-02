8e947a0271ab9f221b70cd4deddf6ca27ed9b2a4
AhmedWaleedAhmed <ahmed.amin98@eng-st.cu.edu.eg>
2023-09-02 16:16:57 +0300

    Session #5 task #4 (python).

---
+++ python/session#05_gui/task_04/server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+################################################### Task #4 description ###################################################
+#  create server receive multiple clients and keep a live
+###########################################################################################################################
+
+# I took a look on multiple resources but I followed this one 
+# https://www.youtube.com/watch?v=xceTFWy_eag
+
+import socket
+import threading
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "bye"
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv(SIZE).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+        msg = f"Msg received: {msg}"
+        msg = input("> ")
+        conn.send(msg.encode(FORMAT))
+
+    conn.close()
+
+def main():
+    print("[STARTING] Server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+if __name__ == "__main__":
+    main()
