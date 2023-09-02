49aace98d8a4bd84e9c4ec2aba6ff2983c08c526
AhmedWaleedAhmed <ahmed.amin98@eng-st.cu.edu.eg>
2023-09-02 17:01:17 +0300

    Session #5 task #2 (python).

---
+++ python/session#05_gui/task_02/task_02.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+################################################### Task #2 description ###################################################
+#  Create a window with a canvas (circle) and change its color when you press on the buttons
+#  We will have two states 1- LED ON 2- LED OFF
+#  Also We will have label to indicate if the LED ON/OFF
+###########################################################################################################################
+
+from tkinter import *
+
+canvas_width = 100
+canvas_height = 100
+radius = 30
+
+def Led_on():
+    canvas.itemconfig(circle, fill='red')
+    label_field['text'] = "Led is ON"
+def Led_off():
+    canvas.itemconfig(circle, fill='white')
+    label_field['text'] = "Led is OFF"
+
+root = Tk()
+root.title("LED ON/OFF")
+root.geometry("300x400")
+frame = Frame(root)
+frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+canvas = Canvas(frame, width=canvas_width, height=canvas_height)
+canvas.pack()
+x0 = (canvas_width / 2) - radius
+y0 = (canvas_height / 2) - radius
+x1 = (canvas_width / 2) + radius
+y1 = (canvas_height / 2) + radius
+circle = canvas.create_oval(x0, y0, x1, y1, width=4, fill='white')
+label_field = Label(root , text ="Led is OFF")
+label_field.place(relx=0.5, rely=0.8, anchor=S)
+button = Button(root, text = "Led ON" , command = Led_on)
+button.place(relx=0.5, rely=0.9, anchor=S)
+button = Button(root, text = "Led OFF" , command = Led_off)
+button.place(relx=0.5, rely=1.0, anchor=S)
+root.mainloop()
