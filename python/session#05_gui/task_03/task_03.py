3ce0b82ceaa0d55c2aff831fff1621eb16cff59c
AhmedWaleedAhmed <ahmed.amin98@eng-st.cu.edu.eg>
2023-09-02 16:28:45 +0300

    Session #5 task #3 (python).

---
+++ python/session#05_gui/task_03/task_03.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+def action():
+    number = int(number_data_field.get())
+    factorial = 1
+    for i in range(1 , number + 1):
+        factorial = factorial*i
+    label_field_result['text'] = f"The factorial of {number} is {number}! = {factorial}"
+    
+    
+root = Tk()
+root.title("Factorial")
+root.geometry("420x150")
+label_field_number  = Label(root , text = "Enter integer value: ")
+label_field_number.place( x = 20 , y = 20)
+number_data_field = Entry(root)
+number_data_field.place( x = 200 , y = 20 , width = 200)
+label_field_result = Label(root , text ="")
+label_field_result.place(x = 200 , y = 50 )
+button = Button(root, text = "Get the result" , command = action)
+button.place(x = 200 , y = 90 , width = 200)
+
+root.mainloop()
